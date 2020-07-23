202cb26dc1415c508f30cc6df7637ff699d1ce41
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2020-07-23 09:13:06 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,59 @@
+# USB Keyboard modification of M.CU.M.E = Multi CompUter Machine Emulator  
+
+All credit to https://github.com/Jean-MarcHarvengt/MCUME for the emulation software and https://www.pjrc.com/teensy/ for the hardware and libraries.
+
+I am currently experimenting with adding a standard usb keyboard to a Teensy 4.0 (first and second photos), Teensy 4.1 (third photo), and a Teensy 3.6 (fourth and fifth photos), using their onboard USB hubs. The Teensy 4.0 used the two bottom USB hub pins D- and D+, as well as an external SDCard adapter. My only interest currently is in modifying and developing the Atari 800/800XL emulations, in part because these are the only working original 1980's hardware (and original OSROMS), I have - also see https://github.com/TobiasVanDyk/Atari600XL-Upgrades.
+
+The code (as inside the AtariTeensy and TeensyAtari folders), is functional for the Teensy 4.1, Teensy 4.0 and 3.6 (and tested running Atari Basic), but is still experimental. The display is an Adafruit ST7789 240x320 IPS display (https://learn.adafruit.com/2-0-inch-320-x-240-color-ips-tft-display).
+
+For the Teensy 4.0 the SDcard adapter on the display was used, and SDCS was connected to pin 8 (Teensy 4.0) and MISO to pin 12 (Teensy 4.0). The files required for this configuration is in the AtariTeensy folder.
+
+
+### Schematic 
+If the built-in SDCard of the Teensy 4.1 or 3.6 is used the the connections to pin 8 and pin 12 are not required. None of the joystick switches are essential as the ROM selection can be done using the usbkeyboard. For all three Teensy's the schematic is then as below, with all possible input and output devices:
+<br>
+<p align="center">
+<img src="schematic2.png" width="900" />  
+<br>
+  
+### Testing (Teensy 4.0):
+<br>
+<p align="center">
+<img src="Teensy40a.png" width="840" />  
+<br>
+
+<br>
+<p align="center">
+<img src="Teensy40b.png" width="840" />  
+<br>
+  
+### Testing (Teensy 4.1):
+<br>
+<p align="center">
+<img src="photo4a.jpg" width="840" />  
+<br>
+  
+### Testing (Teensy 3.6):
+<br>
+<p align="center">
+<img src="photo1a.jpg" width="840" />  
+<br>
+
+<br>
+<p align="center">
+<img src="photo3a.jpg" width="840" />  
+<br>
+
+### Adafruit ST7789 240x320 IPS display 
+From https://learn.adafruit.com/2-0-inch-320-x-240-color-ips-tft-display:
+<br>
+<p align="center">
+<img src="Adafruit 2inch 320x240 Color IPS TFT Display.png" width="640" />  
+<br>
+
+### An alternative solution for the Teensy 4.0 SDCard: 
+From https://hackaday.io/project/171471-pz1-6502-laptop:
+<br>
+<p align="center">
+<img src="teensynano6502.jpg" width="640" />  
+<br>
---
+++ TeensyAtari/TeensyAtari.ino
@@ -0,0 +1,201 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// Atari 800 emulation using a Teensy 4.0's own hub and standard USB keyboard for input. 
+// This is a modification of Jean-MarcHarvengt's Multi CompUter Machine Emulator.
+////////////////////////////////////////////////////////////////////////////////////////
+#include "USBHost_t36.h"
+
+USBHost myusb;
+USBHub hub1(myusb);
+KeyboardController keyboard1(myusb);
+USBHIDParser hid1(myusb);
+USBHIDParser hid2(myusb);
+
+USBDriver *drivers[] = {&hub1, &keyboard1, &hid1, &hid2};
+#define CNT_DEVICES (sizeof(drivers)/sizeof(drivers[0]))
+const char * driver_names[CNT_DEVICES] = {"Hub1", "KB1", "HID1" , "HID2"};
+bool driver_active[CNT_DEVICES] = {false, false, false, false};
+
+extern int usbkey;
+extern int usboldkey;     // Left Right
+extern int usbshiftkey;   // 104  108
+extern int usbcontrolkey; // 103  107
+extern int usbcapslock;   // 57
+extern int usbwindowskey; // 106
+extern int usbaltkey;     // 105 109
+extern int usbarrowup;    // 82 key218
+extern int usbarrowdown;  // 81 key217
+extern int usbarrowright; // 79 key215
+extern int usbarrowleft;  // 80 key216
+extern int SHFLOK;        // Location 702 0, 64 (all uppercase), 128, 196 Shift Ctrl
+extern int usbF6;         // 63 key199
+extern int usbF7;         // 64 key200
+extern int usbF8;         // 65 key201
+extern int usbF9;         // 66 key202
+extern int usbenter;      // 10 key12
+
+extern "C" {
+  #include "iopins.h"  
+  #include "emuapi.h"  
+}
+#include "keyboard_osd.h"
+#include "tft_t_dma.h"
+
+extern "C" {
+#include "atari800.h"
+}
+
+TFT_T_DMA tft = TFT_T_DMA(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO, TFT_TOUCH_CS, TFT_TOUCH_INT);
+
+static IntervalTimer myTimer;
+static elapsedMicros tius;
+
+static unsigned char  palette8[PALETTE_SIZE];
+static unsigned short palette16[PALETTE_SIZE];
+
+volatile boolean vbl = true;
+static int skip = 0;
+static void vblCount() // toggle vbl
+{ if (vbl) vbl = false; else vbl = true; }
+
+void emu_DrawVsync(void)
+{ volatile boolean vb=vbl;
+  skip += 1;
+  skip &= VID_FRAME_SKIP;
+  while (vbl==vb) {}; // in loop until vbl changed external?
+}
+
+void emu_DrawLine(unsigned char * VBuf, int width, int height, int line) 
+{ tft.writeLine(width, 1, line, VBuf, palette16); }  
+
+void emu_DrawScreen(unsigned char * VBuf, int width, int height, int stride) 
+{ if (skip==0) { tft.writeScreen(width, height-TFT_VBUFFER_YCROP, stride, VBuf+(TFT_VBUFFER_YCROP/2)*stride, palette16); } }
+
+int emu_FrameSkip(void)
+{ return skip; }
+
+void * emu_LineBuffer(int line)
+{ return (void*)tft.getLineBuffer(line); }
+
+void emu_SetPaletteEntry(unsigned char r, unsigned char g, unsigned char b, int index)
+{  if (index<PALETTE_SIZE) 
+      { palette8[index]  = RGBVAL8(r,g,b);
+        palette16[index] = RGBVAL16(r,g,b);    
+      }
+}
+
+///////////////////////////////////////////////////////
+// setup runs once on sketch start
+///////////////////////////////////////////////////////
+void setup() 
+{
+  tft.begin();
+   
+  usboldkey = 255;
+  usbkey = 255;
+  usbshiftkey = 0;
+  usbcontrolkey = 0;
+  usbcapslock = 64;
+  usbarrowdown = 0 ;
+  usbarrowup = 0 ;
+  usbarrowright = 0 ;
+  usbarrowleft = 0 ;
+  usbenter = 0;
+  
+  emu_init(); 
+  
+  myTimer.begin(vblCount, 20000);  //to run every 20ms
+
+  myusb.begin();
+  
+  keyboard1.attachPress(OnPress);
+  keyboard1.attachRawPress(OnRawPress);
+  keyboard1.attachRawRelease(OnRawRelease);
+
+}
+
+///////////////////////////////////////////////////////
+// Main Loop 
+// 1. menuActive => initial ROM selection
+// 2. Then input from keyboard or buttons
+///////////////////////////////////////////////////////
+void loop(void) 
+{
+  // 1. ///////////////////////////////////////////////
+  if (menuActive()) 
+     { uint16_t bClick = emu_DebounceLocalKeys();
+       int action = handleMenu(bClick);
+       char * filename = menuSelection();    
+       if (action == ACTION_RUNTFT) 
+          { toggleMenu(false);     // Switch off initial menu
+            emu_Init(filename);
+            tft.fillScreenNoDma( RGBVAL16(0x00,0x00,0x00) );
+            tft.startDMA(); 
+          } 
+     // 2. ///////////////////////////////////////////////   
+     } else                                             // if (!menuActive()) 
+     { emu_Step();                                      // same as at8_Step
+       uint16_t bClick = emu_DebounceLocalKeys();
+       emu_Input(bClick);      
+     }
+  
+  myusb.Task();   // see teensy_usbhost
+
+}
+
+
+////////////// HAS_SND Start ////////////////////////
+#ifdef HAS_SND
+
+#include <Audio.h>
+#include "AudioPlaySystem.h"
+AudioPlaySystem mymixer;
+
+#if defined(__IMXRT1052__) || defined(__IMXRT1062__)    
+    AudioOutputMQS  mqs;
+    AudioConnection patchCord9(mymixer, 0, mqs, 1);
+#else
+    AudioOutputAnalog dac1;
+    AudioConnection   patchCord1(mymixer, dac1);
+#endif
+
+void emu_sndInit() 
+{ Serial.println("Sound init");  
+  AudioMemory(16);
+  mymixer.start();
+}
+
+void emu_sndPlaySound(int chan, int volume, int freq)
+{ if (chan < 6) mymixer.sound(chan, freq, volume); }
+
+void emu_sndPlayBuzz(int size, int val) 
+{ mymixer.buzz(size,val); }
+
+#endif
+////////////// HAS_SND End ////////////////////////
+
+void OnRawPress(uint8_t keycode)
+{ Serial.print("raw key press: ");
+  Serial.println((int)keycode);
+
+  if (((keycode==104) || (keycode==108)) && (usbshiftkey==0))   usbshiftkey   = keycode; // Only stays >0 while pressed down
+  if (((keycode==103) || (keycode==107)) && (usbcontrolkey==0)) usbcontrolkey = keycode; // Only stays >0 while pressed down
+  if (keycode==57) { if (usbcapslock==64)  usbcapslock = 0; else usbcapslock = 64; }     // Toggle 64 or 0 CAPSLOCK 60 3C 0011 1100
+  if (keycode==40) usbenter = 1;
+}
+
+void OnRawRelease(uint8_t keycode)
+{ Serial.print("raw key release: ");
+  Serial.println((int)keycode);
+
+  if (((keycode==104) || (keycode==108)) && (usbshiftkey!=0))  usbshiftkey = 0;     // Only stays >0 while pressed down
+  if (((keycode==103) || (keycode==107)) && (usbcontrolkey!=0)) usbcontrolkey = 0;  // Only stays >0 while pressed down
+}
+
+void OnPress(int key)
+{ usbkey = key;                  // Also used in main loop
+  usboldkey = 255;               // Can then press the same key more than once
+  if (key==215) { usbarrowright = 1 ; }
+  if (key==216) { usbarrowleft = 1 ; }
+  if (key==217) { usbarrowdown = 1 ; }
+  if (key==218) { usbarrowup = 1 ; }
+}
